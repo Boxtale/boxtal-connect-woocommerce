249a77e62e240f39028cb61da667ad387d420ed9
Arnaud Dutant <arnaud@boxtal.com>
2018-02-06 16:57:46 +0100

    use coveralls

--- test/bin/phpunit.sh
+++ test/bin/phpunit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ ${RUN_CODE_COVERAGE} == 1 ]]; then
-	./vendor/bin/phpunit -c phpunit.xml --coverage-clover=clover.xml
+	./vendor/bin/phpunit -c phpunit.xml --coverage-clover=coverage.xml
 else
 	./vendor/bin/phpunit -c phpunit.xml
 fi
