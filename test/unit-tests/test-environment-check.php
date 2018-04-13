48c3ac0f2738f121f72545f8cdceb103235ff09f
Arnaud Dutant <arnaud@boxtal.com>
2018-04-13 13:11:57 +0200

    test bug php

--- test/unit-tests/test-environment-check.php
+++ test/unit-tests/test-environment-check.php
@@ -38,8 +38,8 @@ class BW_Test_Environment_Check extends WC_Unit_Test_Case {
 	 */
 	public function test_get_environment_warning() {
 		$environment_check = new Environment_Check( $this->plugin );
-		$this->assertFalse(
-			$environment_check->boxtal_woocommerce_get_environment_warning()
+		$this->assertEquals(
+			$environment_check->boxtal_woocommerce_get_environment_warning(), false
 		);
 	}
 }
