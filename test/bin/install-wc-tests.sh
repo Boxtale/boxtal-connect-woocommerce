7ca8208df11ab3a896713a88de2329ae7d590fd6
Arnaud Dutant <arnaud@boxtal.com>
2018-04-30 16:46:24 +0200

    test e2e

--- test/bin/install-wc-tests.sh
+++ test/bin/install-wc-tests.sh
@@ -11,6 +11,7 @@ DB_PASS=$3
 DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
 WC_VERSION=${6-"2.6.14"}
+E2E_TEST_DIR='/tmp/wc-e2e-page-objects-tutorial'
 
 clean_directories() {
     rm -rf /tmp/woocommerce
@@ -28,6 +29,65 @@ install_wc() {
 
 install_wp() {
 	bash /tmp/woocommerce/tests/bin/install.sh $DB_NAME $DB_USER "$DB_PASS" $DB_HOST $WP_VERSION
+}
+
+install_e2e_tests() {
+    # Script Variables
+    CONFIG_DIR="./test/e2e/config/travis"
+    WP_CORE_DIR="$HOME/wordpress"
+    NGINX_DIR="$HOME/nginx"
+    PHP_FPM_BIN="$HOME/.phpenv/versions/$TRAVIS_PHP_VERSION/sbin/php-fpm"
+    PHP_FPM_CONF="$NGINX_DIR/php-fpm.conf"
+    WP_SITE_URL="http://localhost:8080"
+    BRANCH=$TRAVIS_BRANCH
+    REPO=$TRAVIS_REPO_SLUG
+    WP_DB_DATA="$HOME/build/$REPO/tests/e2e-tests/data/e2e-db.sql"
+    WORKING_DIR="$PWD"
+
+    if [ "$TRAVIS_PULL_REQUEST_BRANCH" != "" ]; then
+        BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+        REPO=$TRAVIS_PULL_REQUEST_SLUG
+    fi
+
+    set -ev
+    npm install
+    export NODE_CONFIG_DIR="./tests/e2e-tests/config"
+
+    # Set up nginx to run the server
+    mkdir -p "$WP_CORE_DIR"
+    mkdir -p "$NGINX_DIR"
+    mkdir -p "$NGINX_DIR/sites-enabled"
+    mkdir -p "$NGINX_DIR/var"
+
+    cp "$CONFIG_DIR/travis_php-fpm.conf" "$PHP_FPM_CONF"
+
+    # Start php-fpm
+    "$PHP_FPM_BIN" --fpm-config "$PHP_FPM_CONF"
+
+    # Copy the default nginx config files.
+    cp "$CONFIG_DIR/travis_nginx.conf" "$NGINX_DIR/nginx.conf"
+    cp "$CONFIG_DIR/travis_fastcgi.conf" "$NGINX_DIR/fastcgi.conf"
+    cp "$CONFIG_DIR/travis_default-site.conf" "$NGINX_DIR/sites-enabled/default-site.conf"
+
+    # Start nginx.
+    nginx -c "$NGINX_DIR/nginx.conf"
+
+    # Set up WordPress using wp-cli
+    cd "$WP_CORE_DIR"
+
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    php wp-cli.phar core download --version=$WP_VERSION
+    php wp-cli.phar core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --dbprefix=wp_ --extra-php <<PHP
+/* Change WP_MEMORY_LIMIT to increase the memory limit for public pages. */
+define('WP_MEMORY_LIMIT', '256M');
+PHP
+    php wp-cli.phar core install --url="$WP_SITE_URL" --title="Example" --admin_user=admin --admin_password=password --admin_email=info@example.com --path=$WP_CORE_DIR --skip-email
+    php wp-cli.phar db import $WP_DB_DATA
+    php wp-cli.phar search-replace "http://local.wordpress.test" "$WP_SITE_URL"
+    php wp-cli.phar theme install twentytwelve --activate
+    php wp-cli.phar plugin install https://github.com/$REPO/archive/$BRANCH.zip --activate
+
+    cd "$WORKING_DIR"
 }
 
 clean_directories
