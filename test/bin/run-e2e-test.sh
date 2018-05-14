92f76d6bb2bb5e88e08d6adc32cf76a702a732c0
Arnaud Dutant <arnaud@boxtal.com>
2018-05-14 17:18:16 +0200

    fix build

--- test/bin/run-e2e-test.sh
+++ test/bin/run-e2e-test.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+TRAVIS=${1-false}
+
 # Start xvfb to run the tests
 export BASE_URL="http://localhost:80"
-export DISPLAY=:99.0
 
 # Run the tests
-xvfb-run npm test
+if [ ${TRAVIS} = "false" ]; then
+    xvfb-run npm test
+else
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+ 	sleep 3
+	npm test
+fi
+
