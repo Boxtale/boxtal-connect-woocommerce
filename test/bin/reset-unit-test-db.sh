16f6ef33d60d1e4743129811b55dbaa0adcbd9b4
Arnaud Dutant <arnaud@boxtal.com>
2018-05-15 12:45:41 +0200

    build fix

--- test/bin/reset-unit-test-db.sh
+++ test/bin/reset-unit-test-db.sh
@@ -6,7 +6,7 @@ DB_PASS=dbpass
 
 # drop database
 #mysqladmin drop -bf $DB_NAME --user="$DB_USER" --password="$DB_PASS"
-mysql --user="$DB_USER" --password="$DB_PASS" -Bse "DROP DATABASE IF EXISTS $DB_NAME"
+mysql --user="$DB_USER" --password="$DB_PASS" -Bse "DROP DATABASE IF EXISTS ${DB_NAME};"
 
 # create database
 mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"
