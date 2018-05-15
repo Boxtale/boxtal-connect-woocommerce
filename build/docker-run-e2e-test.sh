5adb79f15151ab77e911f1408dc0bff02bffb4a4
Arnaud Dutant <arnaud@boxtal.com>
2018-05-15 14:47:06 +0200

    build fix

--- build/docker-run-e2e-test.sh
+++ build/docker-run-e2e-test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -u docker boxtal_woocommerce /home/docker/test/bin/run-e2e-test.sh
+docker exec -u docker boxtal_woocommerce /home/docker/test/bin/run-e2e-test.sh false
