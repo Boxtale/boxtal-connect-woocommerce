3a916aa019854e326bb6cfacd13365ee1d7cb501
Arnaud Dutant <arnaud@boxtal.com>
2018-05-15 12:27:30 +0200

    build fix

--- test/bin/reset-unit-test-db.sh
+++ test/bin/reset-unit-test-db.sh
@@ -3,26 +3,10 @@
 DB_NAME=boxtal_woocommerce_test
 DB_USER=dbadmin
 DB_PASS=dbpass
-DB_HOST=localhost
-
-# parse DB_HOST for port or socket references
-PARTS=(${DB_HOST//\:/ })
-DB_HOSTNAME=${PARTS[0]};
-DB_SOCK_OR_PORT=${PARTS[1]};
-EXTRA=""
-
-if ! [ -z $DB_HOSTNAME ] ; then
-    if [ $(echo $DB_SOCK_OR_PORT | grep -e '^[0-9]\{1,\}$') ]; then
-        EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
-    elif ! [ -z $DB_SOCK_OR_PORT ] ; then
-        EXTRA=" --socket=$DB_SOCK_OR_PORT"
-    elif ! [ -z $DB_HOSTNAME ] ; then
-        EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
-    fi
-fi
 
 # drop database
-mysqladmin drop -bf $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+#mysqladmin drop -bf $DB_NAME --user="$DB_USER" --password="$DB_PASS"
+mysql --user="$DB_USER" --password="$DB_PASS" -Bse "DROP DATABASE IF EXISTS $DB_NAME"
 
 # create database
-mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"
