22eba0d5534b8ff922f5aead5b67fc9d2998d55e
Arnaud Dutant <arnaud@boxtal.com>
2018-05-18 18:17:58 +0200

    single build

--- test/bin/run-unit-test.sh
+++ test/bin/run-unit-test.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
 docker exec boxtal_woocommerce /home/docker/test/bin/reset-unit-test-db.sh
-
-if [[ ${RUN_CODE_COVERAGE} == 1 ]]; then
-	./vendor/bin/phpunit -c phpunit.xml --coverage-clover=coverage.xml
-else
-	docker exec boxtal_woocommerce /home/docker/vendor/bin/phpunit -c phpunit.xml
-fi
+docker exec boxtal_woocommerce /home/docker/vendor/bin/phpunit -c phpunit.xml
