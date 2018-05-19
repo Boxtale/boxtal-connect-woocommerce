1660fe881e2440d3ba0def2981c93762dcbdfdcb
Arnaud Dutant <arnaud@boxtal.com>
2018-05-19 16:40:43 +0200

    fix coveralls

--- test/bin/run-unit-test.sh
+++ test/bin/run-unit-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker exec boxtal_woocommerce /home/docker/test/bin/reset-unit-test-db.sh
-docker exec boxtal_woocommerce /home/docker/vendor/bin/phpunit -c phpunit.xml
+docker exec boxtal_woocommerce /home/docker/vendor/bin/phpunit -c phpunit.xml --coverage-clover /tmp/coverage.xml
