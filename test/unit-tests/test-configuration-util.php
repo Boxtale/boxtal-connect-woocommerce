45b4f4b706780c4195c6ab7d093b544a841d9384
Arnaud Dutant <arnaud@boxtal.com>
2018-10-03 17:36:21 +0200

    fixed unit tests

--- test/unit-tests/test-configuration-util.php
+++ test/unit-tests/test-configuration-util.php
@@ -18,18 +18,7 @@ class BW_Test_Configuration_Util extends WP_UnitTestCase {
 	 * Test get onboarding link.
 	 */
 	public function test_get_onboarding_link() {
-		$admins = get_super_admins();
-		if ( is_array( $admins ) && count( $admins ) > 0 ) {
-			$admin_user_login = array_shift( $admins );
-			$admin_user       = get_user_by( 'login', $admin_user_login );
-			$admin_user_id    = $admin_user->get( 'ID' );
-		} else {
-			$admin_user_id = 1;
-		}
-
-		$customer = new \WC_Customer( $admin_user_id );
-		Customer_Util::set_email( $customer, 'jsnow@got.com' );
-		Customer_Util::save( $customer );
+		update_option( 'admin_email', 'jsnow@got.com' );
 		update_option( 'siteurl', 'http://xxx.com' );
 		$this->assertSame(
 			Configuration_Util::get_onboarding_link(),
